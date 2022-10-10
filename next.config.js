1e2191007be2cb0d91ae77578547ef13a811f206
0xFable <0xfable@protonmail.com>
2022-10-10 16:45:12 +0100

    fix(state_issues): Fix state issues and display in console
    Note the fix here is just suppressing reported non-issues which are appearing due to a recoil incompat with SSR frameworks. This may supress other warnings but those should be handled directly

--- next.config.js
+++ next.config.js
@@ -4,6 +4,28 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 
 const withBundleJunoblocks = require('next-bundle-junoblocks')
 
+
+/**
+ * @0xFable - Suppress errant recoil errors which seem to occur due to hot reloading 
+ * When using the app there is constant recoil warnings in the logs that users could see which indicate
+ * almost fatal issues with the state. 
+ * Rather than this meaning an issue, on multiple SSR based frameworks there is an issue noted with seeing this on refreshes 
+ * possible because the state/pages are being loaded with hot module replacement
+ * For more info go here: https://github.com/facebookexperimental/Recoil/issues/733
+ */
+const intercept = require("intercept-stdout")
+
+// safely ignore recoil stdout warning messages 
+function interceptStdout(text) {
+  if (text.includes('Duplicate atom key')) {
+    return ''
+  }
+  return text
+}
+
+// Intercept in dev and prod
+intercept(interceptStdout)
+
 const config = {
   async redirects() {
     return [
@@ -56,6 +78,8 @@ const config = {
     return config
   },
 }
+
+
 
 module.exports = withBundleAnalyzer(
   process.env.BUNDLE_JUNOBLOCKS === 'true'
