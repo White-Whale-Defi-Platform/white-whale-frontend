b14b73f51be793f8d814b7ad31bb36b1e13ac783
daiki saito <daiki.saito92@gmail.com>
2022-12-16 21:13:19 +0900

    feat: fix lint and style issues

--- components/Layout/AppLayout.tsx
+++ components/Layout/AppLayout.tsx
@@ -7,23 +7,12 @@ import { walletState } from 'state/atoms/walletAtoms'
 
 import Status from '../Status'
 import RadialGradient from './RadialGradient'
-// import { useRouter } from "next/router";
 
 const AppLayout: FC<ReactNode> = ({ children }) => {
   const { chainId } = useRecoilValue(walletState)
-  // const router = useRouter()
-
-  // useEffect(() => {
-  //   router.replace("/swap")
-  // },[chainId])
 
   return (
-    <Flex
-      direction="column"
-      backgroundColor="transparent"
-      height="100vh"
-      // paddingBottom={8}
-    >
+    <Flex direction="column" backgroundColor="transparent" height="100vh">
       <RadialGradient />
       <Navbar />
       <Flex
