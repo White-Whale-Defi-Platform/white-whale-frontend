dcbca8c81b466a7c862f33b3de3d55aa6f4ca383
daiki saito <daiki.saito92@gmail.com>
2022-12-16 19:28:01 +0900

    feat: add chain info on flash loan page

--- components/Pages/Flashloan/Flashloan.tsx
+++ components/Pages/Flashloan/Flashloan.tsx
@@ -1,8 +1,31 @@
+import { FC, useEffect } from 'react'
+
 import { Box, HStack, Text, VStack } from '@chakra-ui/react'
+import { useChains } from 'hooks/useChainInfo'
+import { useRouter } from 'next/router'
+import { useRecoilValue } from 'recoil'
+import { walletState } from 'state/atoms/walletAtoms'
 
 import FlashloanForm from './FlashloanForm'
 
-const Flashloan = () => {
+// eslint-disable-next-line @typescript-eslint/ban-types
+type Props = {}
+
+const Flashloan: FC<Props> = () => {
+  const { address, chainId } = useRecoilValue(walletState)
+  const router = useRouter()
+  const chains = useChains()
+  const chainIdParam = router.query.chainId as string
+
+  useEffect(() => {
+    if (chainId) {
+      const currenChain = chains.find((row) => row.chainId === chainId)
+      if (currenChain && currenChain.label.toLowerCase() !== chainIdParam) {
+        router.push(`/${currenChain.label.toLowerCase()}/flashloan`)
+      }
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [chainId, chainIdParam])
   return (
     <VStack width={{ base: '100%', md: '722px' }} alignItems="center">
       <Box>
