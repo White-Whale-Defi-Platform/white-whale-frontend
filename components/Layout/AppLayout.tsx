f49822759d263421ecedd1b7c827a9f30182c646
optisman <42278459+daisai3@users.noreply.github.com>
2022-12-22 04:27:41 +0900

    feat: add mobile not support modal on mobile devices (#140)

--- components/Layout/AppLayout.tsx
+++ components/Layout/AppLayout.tsx
@@ -1,16 +1,26 @@
 import { FC, ReactNode } from 'react'
+import {
+  BrowserView,
+  isBrowser,
+  isMobile,
+  MobileView,
+} from 'react-device-detect'
 
-import { Flex } from '@chakra-ui/react'
+import { Flex, useMediaQuery } from '@chakra-ui/react'
 import Navbar from 'components/Navbar'
 import { useRecoilValue } from 'recoil'
 import { walletState } from 'state/atoms/walletAtoms'
 
 import Status from '../Status'
+import MobileNotSupportedModal from '../Wallet/Modal/MobileNotSupportedModal'
 import RadialGradient from './RadialGradient'
+
 // import { useRouter } from "next/router";
 
 const AppLayout: FC<ReactNode> = ({ children }) => {
   const { chainId } = useRecoilValue(walletState)
+  const [isMobileView] = useMediaQuery('(max-width: 480px)')
+
   // const router = useRouter()
 
   // useEffect(() => {
@@ -18,29 +28,32 @@ const AppLayout: FC<ReactNode> = ({ children }) => {
   // },[chainId])
 
   return (
-    <Flex
-      direction="column"
-      backgroundColor="transparent"
-      height="100vh"
-      // paddingBottom={8}
-    >
-      <RadialGradient />
-      <Navbar />
+    <>
+      {(isMobile || isMobileView) && <MobileNotSupportedModal />}
       <Flex
-        key={chainId}
-        justifyContent="center"
-        mx="auto"
-        maxWidth="container.xl"
-        marginBottom={20}
-        width="full"
-        flex="1 1 auto "
+        direction="column"
+        backgroundColor="transparent"
+        height="100vh"
+        // paddingBottom={8}
       >
-        {children}
+        <RadialGradient />
+        <Navbar />
+        <Flex
+          key={chainId}
+          justifyContent="center"
+          mx="auto"
+          maxWidth="container.xl"
+          marginBottom={20}
+          width="full"
+          flex="1 1 auto "
+        >
+          {children}
+        </Flex>
+        <Flex paddingY={10} paddingX={6} alignSelf="flex-end">
+          <Status />
+        </Flex>
       </Flex>
-      <Flex paddingY={10} paddingX={6} alignSelf="flex-end">
-        <Status />
-      </Flex>
-    </Flex>
+    </>
   )
 }
 
