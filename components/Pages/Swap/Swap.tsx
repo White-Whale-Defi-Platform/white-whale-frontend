44729a7f6a13c6e1da40cbbf502c3fe55b0d3521
daiki saito <daiki.saito92@gmail.com>
2022-12-29 00:16:50 +0900

    fix: routing issue

--- components/Pages/Swap/Swap.tsx
+++ components/Pages/Swap/Swap.tsx
@@ -1,4 +1,4 @@
-import { FC, useEffect, useState } from 'react'
+import { FC, useEffect, useState, useMemo } from 'react'
 
 import { HStack, Text, VStack } from '@chakra-ui/react'
 import { useChains } from 'hooks/useChainInfo'
@@ -7,6 +7,7 @@ import { fromChainAmount } from 'libs/num'
 import { useRouter } from 'next/router'
 import { useRecoilState, useRecoilValue } from 'recoil'
 import { walletState } from 'state/atoms/walletAtoms'
+import { usePoolsListQuery } from 'queries/usePoolsListQuery'
 
 import defaultTokens from './defaultTokens.json'
 import useSwap from './hooks/useSwap'
@@ -25,6 +26,8 @@ const Swap: FC<SwapProps> = ({}) => {
   const [reverse, setReverse] = useState<boolean>(false)
   const { chainId, address, key, status } = useRecoilValue(walletState)
   const chains = useChains()
+  const { data: poolList } = usePoolsListQuery()
+
   const [resetForm, setResetForm] = useState<boolean>(false)
   const router = useRouter()
   const currenChain = chains.find((row) => row.chainId === chainId)
@@ -32,21 +35,43 @@ const Swap: FC<SwapProps> = ({}) => {
   const { tx, simulated, state, path, minReceive } = useSwap({ reverse })
   const chainIdParam = router.query.chainId as string
 
+  const tokenList = useMemo(() => {
+    let listObj = {}
+    const { pools = [] } = poolList || {}
+    pools
+      .map(({ pool_assets }) => pool_assets)
+      .map(([a, b]) => {
+        listObj = { ...listObj, [a.symbol]: a, [b.symbol]: b }
+      })
+
+    return Object.keys(listObj).map((row) => {
+      return {
+        symbol: listObj[row].symbol,
+        decimals: listObj[row].decimals,
+        amount: 0,
+      }
+    })
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [poolList])
+
   useEffect(() => {
+    if (!currentChainId) return
     const { from, to } = router.query
 
     if (!from && !to) {
-      if (currentChainId) {
-        const [defaultFrom, defaultTo] = defaultTokens[currentChainId]
-        const params = `?from=${defaultFrom?.tokenSymbol}&to=${defaultTo?.tokenSymbol}`
-        const url = `/${currentChainId}/swap${params}`
+      const [defaultFrom, defaultTo] = defaultTokens[currentChainId]
+      const params = `?from=${defaultFrom?.tokenSymbol}&to=${defaultTo?.tokenSymbol}`
+      const url = `/${currentChainId}/swap${params}`
 
-        setTokenSwapState([defaultFrom, defaultTo])
-        setResetForm(true)
-        router.push(url)
-      }
+      setTokenSwapState([defaultFrom, defaultTo])
+      setResetForm(true)
+      router.push(url)
     } else {
-      const newState: TokenItemState[] = [
+      const [defaultFrom, defaultTo] = defaultTokens[currentChainId]
+
+      // let newState: TokenItemState[]
+
+      let newState: TokenItemState[] = [
         {
           tokenSymbol: String(from),
           amount: 0,
@@ -58,21 +83,35 @@ const Swap: FC<SwapProps> = ({}) => {
           decimals: 6,
         },
       ]
+
+      if (
+        tokenList.find((row) => row.symbol === from) &&
+        tokenList.find((row) => row.symbol === to)
+      ) {
+        return
+      } else {
+        newState = [
+          {
+            tokenSymbol: String(defaultFrom.tokenSymbol),
+            amount: 0,
+            decimals: 6,
+          },
+          {
+            tokenSymbol: String(defaultTo.tokenSymbol),
+            amount: 0,
+            decimals: 6,
+          },
+        ]
+      }
+
       setTokenSwapState(newState)
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [address, chainId, chainIdParam, currentChainId])
+  }, [address, currentChainId, tokenList])
 
   useEffect(() => {
-    if (chainId) {
-      if (currentChainId !== chainIdParam) {
-        router.push(`/${currentChainId}/swap`)
-      }
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [chainId, chainIdParam, address])
+    if (!currentChainId) return
 
-  useEffect(() => {
     if (
       chainIdParam &&
       tokenA?.tokenSymbol !== null &&
--- util/route.ts
+++ util/route.ts
@@ -1,5 +1,7 @@
-export const getPathName = (pathname, newChainLabel) => {
-  return pathname.includes('[chainId')
-    ? pathname.replace('[chainId]', newChainLabel.toLowerCase())
-    : `/${newChainLabel.toLowerCase()}/${pathname}`
+export const getPathName = (router, newChainLabel) => {
+  const paths = router.asPath.split('/')
+
+  return !paths[2]
+    ? `/${newChainLabel.toLowerCase()}/${paths[1]}`
+    : `/${newChainLabel.toLowerCase()}/${paths[2]}`
 }
