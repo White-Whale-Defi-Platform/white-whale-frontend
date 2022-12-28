f3a14f7a232c9c3b890d23b343309c3b3d9c3952
daiki saito <daiki.saito92@gmail.com>
2022-12-28 21:05:13 +0900

    fix: routing issue on flashloan page

--- components/Pages/Flashloan/Flashloan.tsx
+++ components/Pages/Flashloan/Flashloan.tsx
@@ -8,24 +8,15 @@ import { walletState } from 'state/atoms/walletAtoms'
 
 import FlashloanForm from './FlashloanForm'
 
-// eslint-disable-next-line @typescript-eslint/ban-types
-type Props = {}
-
-const Flashloan: FC<Props> = () => {
+const Flashloan: FC = () => {
   const { chainId } = useRecoilValue(walletState)
   const router = useRouter()
   const chains = useChains()
   const chainIdParam = router.query.chainId as string
+  const currenChain = chains.find((row) => row.chainId === chainId)
 
-  useEffect(() => {
-    if (chainId) {
-      const currenChain = chains.find((row) => row.chainId === chainId)
-      if (currenChain && currenChain.label.toLowerCase() !== chainIdParam) {
-        router.push(`/${currenChain.label.toLowerCase()}/flashloan`)
-      }
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [chainId, chainIdParam])
+  if (!currenChain || currenChain.label.toLowerCase() !== chainIdParam)
+    return null
 
   return (
     <VStack width={{ base: '100%', md: '722px' }} alignItems="center">
