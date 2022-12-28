a1f76f3fafe176d0b55f1465af2e3c4bc6062cea
daiki saito <daiki.saito92@gmail.com>
2022-12-29 00:41:48 +0900

    fix: update getPathName function

--- util/route.ts
+++ util/route.ts
@@ -1,7 +1,12 @@
 export const getPathName = (router, newChainLabel) => {
-  const paths = router.asPath.split('/')
+  const pathname = router.pathname
 
-  return !paths[2]
-    ? `/${newChainLabel.toLowerCase()}/${paths[1]}`
-    : `/${newChainLabel.toLowerCase()}/${paths[2]}`
+  return pathname.includes('[chainId')
+    ? pathname.replace('[chainId]', newChainLabel.toLowerCase())
+    : `/${newChainLabel.toLowerCase()}/${pathname}`
+
+  // const paths = router.asPath.split('/')
+  // return !paths[2]
+  //   ? `/${newChainLabel.toLowerCase()}/${paths[1]}`
+  //   : `/${newChainLabel.toLowerCase()}/${paths[2]}`
 }
