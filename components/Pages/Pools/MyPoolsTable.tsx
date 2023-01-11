6d61f8b4ecb58bfbc11a71487981f748e66b00eb
daiki saito <daiki.saito92@gmail.com>
2023-01-11 11:58:15 +0900

    fix: lint issue

--- components/Pages/Pools/MyPoolsTable.tsx
+++ components/Pages/Pools/MyPoolsTable.tsx
@@ -18,14 +18,11 @@ import {
   getCoreRowModel,
   useReactTable,
 } from '@tanstack/react-table'
-import { CHIHUAHUA_MAINNET_CHAIN_ID } from 'constants/chain'
 import { formatPrice } from 'libs/num'
-import { useRecoilValue } from 'recoil'
-import { walletState } from 'state/atoms/walletAtoms'
-import useIgnoreCoinhall from './hooks/useIgnoreCoinhall'
 
 import Loader from '../../Loader'
 import PoolName from './components/PoolName'
+import useIgnoreCoinhall from './hooks/useIgnoreCoinhall'
 import { Pool } from './types'
 
 const columnHelper = createColumnHelper<Pool>()
