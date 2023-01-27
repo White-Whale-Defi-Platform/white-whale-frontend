4a6c623d3c6e412680be6305516a874387e08909
daiki saito <daiki.saito92@gmail.com>
2023-01-28 03:45:58 +0900

    fix: injective apr by dynamic swap fee

--- hooks/useTradingHistory.ts
+++ hooks/useTradingHistory.ts
@@ -4,6 +4,7 @@ import { useQuery } from 'react-query'
 import { gql, request } from 'graphql-request'
 import { useChainInfo } from 'hooks/useChainInfo'
 import { fromChainAmount, num } from 'libs/num'
+import moment from 'moment'
 import { useRecoilValue } from 'recoil'
 import { walletState } from 'state/atoms/walletAtoms'
 import { getTokenPrice } from 'util/coingecko'
@@ -11,7 +12,7 @@ import { getTokenDecimal } from 'util/token'
 
 const query = gql`
   query ($filter: TradingHistoryFilter) {
-    tradingHistories(first: 1000, filter: $filter) {
+    tradingHistories(first: 1000, filter: $filter, orderBy: DATETIME_ASC) {
       nodes {
         id
         datetime
@@ -20,6 +21,7 @@ const query = gql`
         secondToken
         baseVolume
         targetVolume
+        swapFeeVolume
       }
     }
   }
@@ -27,9 +29,9 @@ const query = gql`
 
 export const useTradingHistory = ({ pair, dateTime }) => {
   const [volume, setVolume] = useState<number | undefined>()
+  const [feeVolume, setFeeVolume] = useState<number | undefined>()
   const currentWalletState = useRecoilValue(walletState)
   const [activeChain]: any = useChainInfo(currentWalletState.chainId)
-  const SWAP_FEE = 0.002
 
   const filter = {
     pairAddr: {
@@ -51,25 +53,76 @@ export const useTradingHistory = ({ pair, dateTime }) => {
 
   const calculateVolume = async (data) => {
     if (!data) return 0
-    if (volume !== undefined) return
+    if (volume !== undefined || feeVolume !== undefined) return
 
     const tradingHistories = data?.tradingHistories?.nodes
       ? data?.tradingHistories?.nodes
       : []
 
-    let tradingVolume = 0
+    const historicalDatetimes = [
+      moment(tradingHistories[0]?.datetime).startOf('day').valueOf(),
+      moment(tradingHistories.slice(-1)[0]?.datetime).startOf('day').valueOf(),
+    ]
 
-    await Promise.all(
-      tradingHistories.map(async (row) => {
-        const baseVolume = num(
-          fromChainAmount(row?.targetVolume || 0, getTokenDecimal(row?.token))
-        )
-        const offerAssetPrice = await getTokenPrice(row?.token, row?.datetime)
-        tradingVolume += offerAssetPrice * baseVolume.toNumber()
+    const swapAssets = Array.from(
+      new Set(
+        tradingHistories.map((row) => {
+          return row?.secondToken
+        })
+      )
+    )
+
+    const historicalPricesArray = await Promise.all(
+      swapAssets.map(async (swapAsset: string) => {
+        return {
+          [swapAsset]: await Promise.all(
+            historicalDatetimes.map(async (historicalDatetime: number) => {
+              return {
+                [historicalDatetime]: await getTokenPrice(
+                  swapAsset,
+                  historicalDatetime
+                ),
+              }
+            })
+          ),
+        }
       })
     )
 
+    let historicalPricesObj = {}
+    historicalPricesArray.map((row) => {
+      for (const [key, value] of Object.entries(row)) {
+        historicalPricesObj = {
+          ...historicalPricesObj,
+          [key]: {
+            ...value[0],
+            ...value[1],
+          },
+        }
+      }
+    })
+
+    let tradingVolume = 0
+    let tradingFeeVolume = 0
+    tradingHistories.map((row, i) => {
+      const baseVolume = num(
+        fromChainAmount(row?.baseVolume || 0, getTokenDecimal(row?.secondToken))
+      )
+      const swapFeeVolume = num(
+        fromChainAmount(
+          row?.swapFeeVolume || 0,
+          getTokenDecimal(row?.secondToken)
+        )
+      )
+      const swapDate = moment(row?.datetime).startOf('day').valueOf()
+      const assetPrice = historicalPricesObj[row?.secondToken][swapDate] || 0
+
+      tradingVolume += assetPrice * baseVolume.toNumber()
+      tradingFeeVolume += assetPrice * swapFeeVolume.toNumber()
+    })
+
     setVolume(tradingVolume)
+    setFeeVolume(tradingFeeVolume)
   }
 
   useEffect(() => {
@@ -80,7 +133,7 @@ export const useTradingHistory = ({ pair, dateTime }) => {
 
   return {
     tradingVolume: volume || 0,
-    feeVolume: (volume || 0) * SWAP_FEE,
+    feeVolume: feeVolume || 0,
     isLoading,
   }
 }
