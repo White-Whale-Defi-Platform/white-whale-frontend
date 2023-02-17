d7727a2e7f20a3b8b6a66bac57d44eb809e7bab3
Vinod Hum <vinodhum@outlook.com>
2023-02-17 15:58:55 -0500

    fix whitelist

--- next.config.js
+++ next.config.js
@@ -51,7 +51,7 @@ const config = {
           },
           {
             key: 'Content-Security-Policy',
-            value: "default-src 'self' https://*exquisite-salamander-a1fe5e.netlify.app https://*.whitewhale.money; style-src 'self' https://*exquisite-salamander-a1fe5e.netlify.app https://*.whitewhale.money; form-action 'self' https://*exquisite-salamander-a1fe5e.netlify.app https://*.whitewhale.money; script-src 'self' https://*exquisite-salamander-a1fe5e.netlify.app https://*.whitewhale.money; connect-src 'self' https://*exquisite-salamander-a1fe5e.netlify.app https://*.whitewhale.money; img-src 'self' https://*exquisite-salamander-a1fe5e.netlify.app https://*.whitewhale.money; base-uri 'self' https://*exquisite-salamander-a1fe5e.netlify.app https://*.whitewhale.money;"
+            value: "default-src 'self' https://*.netlify.app https://*.whitewhale.money; style-src 'self' https://*.netlify.app https://*.whitewhale.money; form-action 'self' https://*.netlify.app https://*.whitewhale.money; script-src 'self' https://*.netlify.app https://*.whitewhale.money; connect-src 'self' https://*.netlify.app https://*.whitewhale.money; img-src 'self' https://*.netlify.app https://*.whitewhale.money; base-uri 'self' https://*.netlify.app https://*.whitewhale.money;"
           },
           {
             key: 'X-Content-Type-Options',
