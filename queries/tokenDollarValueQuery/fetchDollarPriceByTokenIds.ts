981c9701b3239bf6e2f83c8ac846d26e913934e3
0xFable <86927513+0xFable@users.noreply.github.com>
2023-03-15 10:58:52 +0000

    feat(pools): Add ampWHALE-WHALE, bWHALE-WHALE, WHALE-HUAHUA (#198)
    * feat(pools): Add ampWHALE-WHALE, bWHALE-WHALE, WHALE-HUAHUA
    
    * fix(NA): No price for ampWHALE and bWHALE, fix chihuahua
    
    * fix(Pools): Ordering on pools
    
    * Fix whale price
    
    * fix coingecko price
    
    * fix whale price
    
    * fix ratio on lp
    
    * fix(HUAHUA-WHALE): If pool is this pool reverse assets on total liq, its getting modified somewhere
    
    ---------
    
    Co-authored-by: Vinod Hum <vinodhum@outlook.com>

--- queries/tokenDollarValueQuery/fetchDollarPriceByTokenIds.ts
+++ queries/tokenDollarValueQuery/fetchDollarPriceByTokenIds.ts
@@ -2,14 +2,16 @@ type ApiResponse = Record<string, { usd: number }>
 
 export const fetchDollarPriceByTokenIds = debounce(
   async (tokenIds: Array<string>): Promise<ApiResponse> => {
-    const apiIds = tokenIds.flat().join(',')
+    const apiIds = tokenIds.flat().filter(token => token !== 'whale-token')
+    const newApiIds = [...new Set(apiIds)].join(',')
 
     try {
       const response = await fetch(
         // `https://api.coingecko.com/api/v3/simple/price?ids=${chainId}&vs_currencies=usd`,
-        `https://api.coingecko.com/api/v3/simple/price?ids=${apiIds}&vs_currencies=usd`,
+        `https://api.coingecko.com/api/v3/simple/price?ids=${newApiIds}&vs_currencies=usd`,
         {
           method: 'GET',
+          cache: "force-cache"
         }
       )
       if (response.status === 200) {
@@ -21,7 +23,7 @@ export const fetchDollarPriceByTokenIds = debounce(
       return {}
     }
   },
-  100
+  1000
 )
 
 function debounce<T extends (args: any) => Promise<any>>(
