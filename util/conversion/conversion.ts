559977b3433669272808e653dfb11fe85a96452d
worrex <68468180+worrex@users.noreply.github.com>
2023-05-03 17:30:53 +0200

    fix(reward-progress-bar): fixed wrong duration
    Adjusted duration string so that it takes single time units into account

--- util/conversion/conversion.ts
+++ util/conversion/conversion.ts
@@ -45,11 +45,11 @@ export const calculateRewardDurationString = (
   const nowInMilli = Date.now()
   const adjustedDurationInMilli =
     nanoToMilli(genesisStartTimeInNano) > nowInMilli
-      ? nanoToMilli(genesisStartTimeInNano) + 86_000_000 - nowInMilli
+      ? nanoToMilli(genesisStartTimeInNano) - nowInMilli
       : durationInMilli
 
   useEffect(() => {
-    if (adjustedDurationInMilli <= 1000) {
+    if (adjustedDurationInMilli <= 1_000) {
       setImminent(true)
     } else {
       setImminent(false)
@@ -58,26 +58,34 @@ export const calculateRewardDurationString = (
 
   if (isImminent) {
     return `imminent`
-  } else if (adjustedDurationInMilli >= 86400_000) {
-    return `${Math.floor(adjustedDurationInMilli / 86400_000)} days`
-  } else if (adjustedDurationInMilli >= 3600_000) {
-    return `${Math.floor(adjustedDurationInMilli / 3600_000)} hours`
+  } else if (adjustedDurationInMilli >= 86_400_000) {
+    const days = Math.floor(adjustedDurationInMilli / 86_400_000)
+    return  days === 1 ? `${days} day`: `${days} days`
+  } else if (adjustedDurationInMilli >= 3_600_000) {
+    const hours = Math.floor(adjustedDurationInMilli / 3_600_000)
+    return  hours === 1 ?  `${hours} hour`: `${hours} hours`
   } else if (adjustedDurationInMilli >= 60_000) {
-    return `${Math.floor(adjustedDurationInMilli / 60_000)} minutes`
+    const minutes = Math.floor(adjustedDurationInMilli / 60_000)
+    return minutes === 1 ? `${minutes} minute`:`${minutes} minutes`
   } else if (adjustedDurationInMilli > 1_000) {
-    return `${Math.floor(adjustedDurationInMilli / 1_000)} seconds`
+    const seconds = Math.floor(adjustedDurationInMilli / 1_000)
+    return seconds === 1 ? `${seconds} second`: `${seconds} seconds`
   }
 }
 
 export const calculateDurationString = (durationInMilli: number): string => {
-  if (durationInMilli >= 86400000) {
-    return `${Math.floor(durationInMilli / 86400_000)} days`
-  } else if (durationInMilli >= 3600_000) {
-    return `${Math.floor(durationInMilli / 3600_000)} hours`
+  if (durationInMilli >= 86_400_000) {
+    const days = Math.floor(durationInMilli / 86_400_000)
+    return  days === 1 ? `${days} day`: `${days} days`
+  } else if (durationInMilli >= 3_600_000) {
+    const hours = Math.floor(durationInMilli / 3_600_000)
+    return  hours === 1 ?  `${hours} hour`: `${hours} hours`
   } else if (durationInMilli >= 60_000) {
-    return `${Math.floor(durationInMilli / 60_000)} minutes`
+    const minutes = Math.floor(durationInMilli/ 60_000)
+    return minutes === 1 ? `${minutes} minute`:`${minutes} minutes`
   } else if (durationInMilli > 1_000) {
-    return `${Math.floor(durationInMilli / 1_000)} seconds`
+    const seconds = Math.floor(durationInMilli / 1_000)
+    return seconds === 1 ? `${seconds} second`: `${seconds} seconds`
   } else {
     return `imminent`
   }
