8dd047f1df396b1e3cfe706d8dbf0fa2deeaf014
worrex <68468180+worrex@users.noreply.github.com>
2023-05-03 17:40:25 +0200

    fix(formatting): ran yarn prettier "**/*.{ts,md,tsx}" --write

--- util/conversion/conversion.ts
+++ util/conversion/conversion.ts
@@ -60,32 +60,32 @@ export const calculateRewardDurationString = (
     return `imminent`
   } else if (adjustedDurationInMilli >= 86_400_000) {
     const days = Math.floor(adjustedDurationInMilli / 86_400_000)
-    return  days === 1 ? `${days} day`: `${days} days`
+    return days === 1 ? `${days} day` : `${days} days`
   } else if (adjustedDurationInMilli >= 3_600_000) {
     const hours = Math.floor(adjustedDurationInMilli / 3_600_000)
-    return  hours === 1 ?  `${hours} hour`: `${hours} hours`
+    return hours === 1 ? `${hours} hour` : `${hours} hours`
   } else if (adjustedDurationInMilli >= 60_000) {
     const minutes = Math.floor(adjustedDurationInMilli / 60_000)
-    return minutes === 1 ? `${minutes} minute`:`${minutes} minutes`
+    return minutes === 1 ? `${minutes} minute` : `${minutes} minutes`
   } else if (adjustedDurationInMilli > 1_000) {
     const seconds = Math.floor(adjustedDurationInMilli / 1_000)
-    return seconds === 1 ? `${seconds} second`: `${seconds} seconds`
+    return seconds === 1 ? `${seconds} second` : `${seconds} seconds`
   }
 }
 
 export const calculateDurationString = (durationInMilli: number): string => {
   if (durationInMilli >= 86_400_000) {
     const days = Math.floor(durationInMilli / 86_400_000)
-    return  days === 1 ? `${days} day`: `${days} days`
+    return days === 1 ? `${days} day` : `${days} days`
   } else if (durationInMilli >= 3_600_000) {
     const hours = Math.floor(durationInMilli / 3_600_000)
-    return  hours === 1 ?  `${hours} hour`: `${hours} hours`
+    return hours === 1 ? `${hours} hour` : `${hours} hours`
   } else if (durationInMilli >= 60_000) {
-    const minutes = Math.floor(durationInMilli/ 60_000)
-    return minutes === 1 ? `${minutes} minute`:`${minutes} minutes`
+    const minutes = Math.floor(durationInMilli / 60_000)
+    return minutes === 1 ? `${minutes} minute` : `${minutes} minutes`
   } else if (durationInMilli > 1_000) {
     const seconds = Math.floor(durationInMilli / 1_000)
-    return seconds === 1 ? `${seconds} second`: `${seconds} seconds`
+    return seconds === 1 ? `${seconds} second` : `${seconds} seconds`
   } else {
     return `imminent`
   }
