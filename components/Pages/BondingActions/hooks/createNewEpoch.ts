74d1823d215d3e7006bd36f1a0b0fa8d6a2839ad
Worrex <68468180+worrex@users.noreply.github.com>
2023-05-09 16:50:31 +0200

    feat(force-epoch-button): add force epoch button (#234)

---
+++ components/Pages/BondingActions/hooks/createNewEpoch.ts
@@ -0,0 +1,13 @@
+import { Wallet } from 'util/wallet-adapters'
+import { Config } from 'components/Pages/Dashboard/hooks/useDashboardData'
+
+export const createNewEpoch = async (
+  client: Wallet,
+  config: Config,
+  address: string
+) => {
+  const handleMsg = {
+    new_epoch: {},
+  }
+  return client.execute(address, config.fee_distributor_address, handleMsg)
+}
--- components/Pages/Dashboard/BondingOverview.tsx
+++ components/Pages/Dashboard/BondingOverview.tsx
@@ -19,6 +19,7 @@ export enum ActionType {
   unbond,
   withdraw,
   claim,
+  createNewEpoch,
 }
 
 export enum WhaleType {
