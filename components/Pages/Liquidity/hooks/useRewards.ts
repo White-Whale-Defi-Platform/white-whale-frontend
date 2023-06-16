dca3157631a5b06a46183cbcaadf7f20ad5e7581
Worrex <68468180+worrex@users.noreply.github.com>
2023-06-16 15:51:39 +0200

    fix(daily-estimated-rewards): were not displayed correctly (#254)
    * fix(daily-estimated-rewards): were not displayed correctly
    
    * fix(available-rewards): aggregate same token rewards
    
    * feat(format-pre-commit): run yarn prettier before commit
    
    * fix(format)

--- components/Pages/Liquidity/hooks/useRewards.ts
+++ components/Pages/Liquidity/hooks/useRewards.ts
@@ -8,26 +8,6 @@ import { useRecoilValue } from 'recoil'
 import { walletState } from 'state/atoms/walletAtoms'
 import { useQueryPoolLiquidity } from 'queries/useQueryPools'
 
-const rewardsMock = [
-  {
-    amount: '1000000',
-    info: {
-      token: {
-        contract_addr:
-          'migaloo1j08452mqwadp8xu25kn9rleyl2gufgfjnv0sn8dvynynakkjukcq5u780c',
-      },
-    },
-  },
-  {
-    amount: '2000000',
-    info: {
-      native_token: {
-        denom: 'uwhale',
-      },
-    },
-  },
-]
-
 type RewardData = {
   amount: string
   info: {
@@ -50,6 +30,34 @@ export type RewardsResult = {
   totalValue: number
 }
 
+function aggregateRewards(rewards: RewardData[]): RewardData[] {
+  // Use reduce to create the aggregates
+  const aggregates = rewards.reduce((acc: { [id: string]: number }, reward) => {
+    // Use contract_addr if it exists, otherwise use denom
+    const denom =
+      reward.info.token?.contract_addr || reward.info.native_token?.denom
+
+    // If neither contract_addr nor denom exist, skip this reward
+    if (!denom) return acc
+
+    const amount = parseInt(reward.amount)
+    acc[denom] = (acc[denom] || 0) + amount
+    return acc
+  }, {})
+
+  // Use Object.entries and map to transform aggregates into an array of rewards
+  return Object.entries(aggregates).map(([id, amount]) => {
+    // Determine whether id is a contract_addr or a denom
+    const isContract = id.startsWith('contract:')
+
+    return {
+      amount: amount.toString(),
+      info: isContract
+        ? { token: { contract_addr: id } }
+        : { native_token: { denom: id } },
+    }
+  })
+}
 const useRewards = (poolId) => {
   const [tokenList] = useTokenList()
   const prices = usePrices()
@@ -58,7 +66,7 @@ const useRewards = (poolId) => {
   const { address, client } = useRecoilValue(walletState)
 
   const { data: rewards = [] } = useQuery({
-    queryKey: ['rewards'],
+    queryKey: ['rewards', staking_address, address],
     queryFn: async (): Promise<RewardData[]> => {
       // return Promise.resolve(rewardsMock)
       return client?.queryContractSmart(staking_address, {
@@ -68,11 +76,12 @@ const useRewards = (poolId) => {
     select: (data) => data?.rewards || [],
     enabled: !!staking_address && !!address,
   })
+  const aggregatedRewards = useMemo(() => aggregateRewards(rewards), [rewards])
 
   return useMemo(() => {
     const rewardsWithToken = []
 
-    rewards?.forEach((reward) => {
+    aggregatedRewards?.forEach((reward) => {
       //cw20 token
       if (reward.info.token) {
         const t = tokenList?.tokens.find(
@@ -113,7 +122,7 @@ const useRewards = (poolId) => {
         ?.reduce((acc, reward) => acc + reward.dollarValue, 0)
         ?.toFixed(2),
     } as RewardsResult
-  }, [rewards, tokenList, prices])
+  }, [aggregatedRewards, tokenList, prices])
 }
 
 export default useRewards
