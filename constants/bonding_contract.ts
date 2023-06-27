9b8bec07c17ae7cf3140097eb08d80eaabbc181e
worrex <68468180+worrex@users.noreply.github.com>
2023-06-27 11:36:24 +0200

    feature(chihuahua-integration): enabling incentives for chihuahua

--- constants/bonding_contract.ts
+++ constants/bonding_contract.ts
@@ -3,7 +3,11 @@ export const BONDING_ENABLED_CHAIN_IDS = [
   'phoenix-1',
   'chihuahua-1',
 ]
-export const INCENTIVE_ENABLED_CHAIN_IDS = ['narwhal-1', 'phoenix-1']
+export const INCENTIVE_ENABLED_CHAIN_IDS = [
+  'narwhal-1',
+  'phoenix-1',
+  'chihuahua-1',
+]
 export const B_WHALE_TOKEN_SYMBOL = 'bWHALE'
 export const AMP_WHALE_TOKEN_SYMBOL = 'ampWHALE'
 export const WHALE_TOKEN_SYMBOL = 'WHALE'
