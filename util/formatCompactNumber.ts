ef4df2abe70fccc3cda6330575b3098c8590f185
Sen Com <davidgraf@Air-von-David.fritz.box>
2023-07-13 13:52:53 +0200

    chore: refactor util/formatCompactNumber.ts

--- util/formatCompactNumber.ts
+++ util/formatCompactNumber.ts
@@ -4,7 +4,6 @@ const formatWithOneDecimal = new Intl.NumberFormat('en-US', {
   minimumFractionDigits: 0,
   maximumFractionDigits: 1,
 })
-
 const oneMillion = 1000000
 const hundredMillions = 100 * oneMillion
 
@@ -14,22 +13,15 @@ export const formatCompactNumber = (
 ) => {
   if (value > hundredMillions) {
     return `${Math.round(value / hundredMillions)}M`
-  }
-  if (value > oneMillion) {
+  } else if (value > oneMillion) {
     return `${formatWithOneDecimal.format(value / oneMillion)}M`
-  }
-  if (value > 10000) {
+  } else if (value > 10000) {
     return `${formatWithOneDecimal.format(Math.round(value / 1000))}K`
-  }
-  if (value > 1000) {
+  } else if (value > 1000 || kind === 'dollarValue') {
     return dollarValueFormatter(Math.round(value), {
       includeCommaSeparation: true,
     })
+  } else {
+    return formatTokenBalance(value)
   }
-  if (kind === 'dollarValue') {
-    return dollarValueFormatter(value, {
-      includeCommaSeparation: true,
-    })
-  }
-  return formatTokenBalance(value)
 }
