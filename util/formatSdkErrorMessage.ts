5820fe3d6eaef185f0866c4d897c8852f278f06a
Sen Com <86837735+Sen-Com@users.noreply.github.com>
2023-07-21 13:57:16 +0200

    Chore/remove dead code (#313)
    * chore: refactor util/truncate.ts
    
    * chore: refactor util/token.ts
    
    * chore: refactor util/route.ts
    
    * chore: refactor util/parseError.ts
    
    * chore: refactor util/localStorageEffect.ts
    
    * chore: refactor util/formatSdkErrorMessage.ts
    
    * chore: refactor util/formatCompactNumber.ts
    
    * chore: refactor util/externalLinkPopup.ts
    
    * chore: refactor util/coingecko.ts
    
    * chore: remove dead code
    
    * fix: restore logs.js
    
    ---------
    
    Co-authored-by: Sen Com <davidgraf@Air-von-David.fritz.box>
    Co-authored-by: Sen Com <davidgraf@MacBook-Air-von-David.fritz.box>

--- util/formatSdkErrorMessage.ts
+++
@@ -1,7 +0,0 @@
-export function formatSdkErrorMessage(e) {
-  return String(e).length > 300
-    ? `${String(e).substring(0, 150)} ... ${String(e).substring(
-        String(e).length - 150
-      )}`
-    : String(e)
-}
--- util/token.ts
+++ util/token.ts
@@ -1,19 +1,8 @@
 export const getTokenDecimal = (address: string) => {
-  let tokenCGCId = 6
-
-  switch (address) {
-    case 'peggy0xdAC17F958D2ee523a2206206994597C13D831ec7':
-      // raw
-      tokenCGCId = 6
-      break
-    case 'inj':
-      // raw
-      tokenCGCId = 18
-      break
-
-    default:
-      break
+  const tokenDecimalMap = {
+    peggy0xdAC17F958D2ee523a2206206994597C13D831ec7: 6,
+    inj: 18,
   }
 
-  return tokenCGCId
+  return tokenDecimalMap[address] || 6
 }
