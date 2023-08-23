4fb296c90b3af493082d9de4921770825ea27602
Sen Com <86837735+Sen-Com@users.noreply.github.com>
2023-08-24 00:38:36 +0200

    Chore/unimported (#352)
    * chore: remove bech32
    
    * chore: uninstall color
    
    * chore: uninstall body-scroll-lock
    
    * chore: uninstall focus-visible
    
    * chore: uninstall gsap
    
    * chore: remove dead code
    
    * chore: uninstall moment
    
    * chore: uninstall @stitches/react
    
    * chore: uninstall @react-spring/web
    
    * chore: uninstall react-merge-refs
    
    * chore: uninstall ramda
    
    * chore: uninstall normalize.css
    
    * chore: uninstall eslint-plugin-react
    
    * chore: uninstall eslint-plugin-react-hooks
    
    * chore: uninstall eslint-plugin-simple-import-sort
    
    * chore: uninstall eslint-plugin-prettier
    
    * chore: uninstall eslint-plugin-jsx-a11y
    
    * chore: uninstall eslint-config-prettier
    
    * chore: remove prettier
    
    * chore: uninstall eslint-plugin-import
    
    * chore: uninstall @terra-money/terra.js
    
    * chore: uninstall @walletconnect/client
    
    * chore: uninstall @cosmjs/math
    
    * chore: uninstall @cosmjs/crypto
    
    * chore: remove unresolved import
    
    * chore: uninstall @cosmjs/cosmwasm
    
    * chore: uninstall @keplr-wallet/wc-qrcode-modal
    
    * chore: uninstall injectivejs
    
    * chore: uninstall eslint-plugin-import-helpers
    
    * feat: add unimported config
    
    * feat: add unimported script
    
    * feat: add unimported script to pre-commit hook
    
    * chore: update yarn.lock
    
    ---------
    
    Co-authored-by: Sen Com <davidgraf@MacBook-Air-von-David.fritz.box>
    Co-authored-by: Sen Com <davidgraf@Air-von-David.fritz.box>

--- util/token.ts
+++
@@ -1,8 +0,0 @@
-export const getTokenDecimal = (address: string) => {
-  const tokenDecimalMap = {
-    peggy0xdAC17F958D2ee523a2206206994597C13D831ec7: 6,
-    inj: 18,
-  }
-
-  return tokenDecimalMap[address] || 6
-}
