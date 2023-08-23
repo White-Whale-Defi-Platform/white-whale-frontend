c20aba029194361fa00f7d054afe4fc77e45eb88
0xFable <0xfable@protonmail.com>
2023-08-23 14:44:04 +0100

    fix(caching): Add caching to API requests we serve through cors api

--- pages/api/cors.ts
+++ pages/api/cors.ts
@@ -6,6 +6,8 @@ const Cors = async (req, res) => {
   try {
     const _res = await axios.get(url)
     res.status(200).send(_res.data)
+    // Add a cache of 10 seconds to avoid constantly returning the same data
+    res.setHeader('Cache-Control', 's-maxage=10')
   } catch (error) {
     res.status(400).send(error.toString())
   }
