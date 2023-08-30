85f0a1e3643498340e9eebb350a26cd8b174dbfc
worrex <68468180+worrex@users.noreply.github.com>
2023-08-30 20:26:02 +0200

    feat(terra-classic-integration): link fixes and pool styling improvements

--- components/Navbar/Logo.tsx
+++ components/Navbar/Logo.tsx
@@ -1,26 +1,28 @@
 import React from 'react'
 
 import { HStack, Image, Text } from '@chakra-ui/react'
-import Link from 'next/link'
 import { useRecoilState } from 'recoil'
 import { walletState } from 'state/atoms/walletAtoms'
+import { useRouter } from 'next/router'
 
 const Logo = () => {
   const [currentWalletState, setCurrentWalletState] =
     useRecoilState(walletState)
+  const router = useRouter()
   return (
     <HStack alignItems="center">
-      <Link href="/">
         <a
-          onClick={() => setCurrentWalletState({
-            ...currentWalletState,
-            chainId: 'migaloo-1',
-          })
+          onClick={async() => {
+            setCurrentWalletState({
+              ...currentWalletState,
+              chainId: 'migaloo-1'
+            })
+            await router.push('/migaloo/swap')
+          }
           }
         >
           <Image src="/img/logo.svg" alt="WhiteWhale Logo" boxSize={[8, 12]} />
         </a>
-      </Link>
       <HStack display={['none', 'flex']}>
         <Text pl={2} fontSize="26" fontWeight="400">
           White
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,4 @@
-import { useEffect } from 'react'
-
-import { NextRouter, useRouter } from 'next/router'
-
 const Index = () => {
-  const router: NextRouter = useRouter()
-
-  /*
-   * UseEffect(() => {
-   *   router.replace('/migaloo/swap')
-   *   // eslint-disable-next-line react-hooks/exhaustive-deps
-   * }, [])
-   */
-
   return <div></div>
 }
-
 export default Index
