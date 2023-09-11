e8a91ecbebd3bf488175274ec81a0a8105403e1c
worrex <68468180+worrex@users.noreply.github.com>
2023-09-11 19:10:58 +0200

    fix(withdraw-locked-pos): enabled unlock locked lp

--- components/Pages/Trade/Liquidity/Action.tsx
+++ components/Pages/Trade/Liquidity/Action.tsx
@@ -2,12 +2,13 @@ import { Box, Button } from '@chakra-ui/react'
 
 import { useClosePosition } from 'components/Pages/Trade/Liquidity/hooks/useClosePosition'
 import { useWithdrawPosition } from 'components/Pages/Trade/Liquidity/hooks/useWithdrawPosition'
+import { STATE } from 'components/Pages/Trade/Liquidity/Positions'
 
 export const Action = ({ item, poolId }) => {
   const close = useClosePosition({ poolId })
   const withdraw = useWithdrawPosition({ poolId })
 
-  if (item?.state === 'active') {
+  if (item?.state === STATE.ACTIVE) {
     return (
       <Button
         width="full"
@@ -20,13 +21,13 @@ export const Action = ({ item, poolId }) => {
         Close
       </Button>
     )
-  } else if (item?.state === 'unbonding') {
+  } else if (item?.state === STATE.UNBONDING) {
     return (
       <Button width="full" variant="outline" size="sm" isDisabled={true}>
         Unbonding
       </Button>
     )
-  } else if (item?.state === 'unbound') {
+  } else if (item?.state === STATE.UNBONDED) {
     return (
       <Button
         width="full"
@@ -36,6 +37,18 @@ export const Action = ({ item, poolId }) => {
         onClick={() => withdraw?.submit()}
       >
         Unbound
+      </Button>
+    )
+  } else if (item?.state === STATE.WITHDRAW) {
+    return (
+      <Button
+        width="full"
+        variant="primary"
+        size="sm"
+        isLoading={withdraw?.isLoading}
+        onClick={() => withdraw?.submit()}
+      >
+       Unlock LP
       </Button>
     )
   }
--- components/Pages/Trade/Liquidity/Positions.tsx
+++ components/Pages/Trade/Liquidity/Positions.tsx
@@ -4,54 +4,58 @@ import { Box, Button, Divider, HStack } from '@chakra-ui/react'
 
 import { PositionsTable } from 'components/Pages/Trade/Liquidity/PositionsTable'
 
-const STATES = ['all', 'active', 'unbonding']
+export enum STATE {
+  ALL = 'all',
+  ACTIVE = 'active',
+  WITHDRAW = 'withdraw',
+  UNBONDED = 'unbonded',
+  UNBONDING = 'unbonding'
+}
 
 type Props = {
   positions: any[]
 }
 
 export const Positions = ({ positions }: Props) => {
-  const [activeButton, setActiveButton] = useState('all')
+  const [activeButton, setActiveButton] = useState(STATE.ALL)
   const [columnFilters, setColumnFilters] = useState([])
 
   return (
-    <Box width="full">
+    <Box width='full'>
       <HStack
-        margin="20px"
-        backgroundColor="rgba(0, 0, 0, 0.25)"
-        width="auto"
-        px="24px"
-        py="12px"
-        borderRadius="75px"
-        gap="20px"
+        margin='20px'
+        backgroundColor='rgba(0, 0, 0, 0.25)'
+        width='auto'
+        px='24px'
+        py='12px'
+        borderRadius='75px'
+        gap='20px'
       >
-        {STATES.map((item) => (
+        {[ STATE.ALL, STATE.ACTIVE, STATE.UNBONDING, STATE.WITHDRAW ].map((item) => (
           <Button
             key={item}
-            minW="120px"
+            minW='120px'
             variant={activeButton === item ? 'primary' : 'unstyled'}
-            color="white"
-            size="sm"
+            color='white'
+            size='sm'
             onClick={() => {
               setActiveButton(item)
-              setColumnFilters(item === 'all'
+              setColumnFilters(item === STATE.ALL
                 ? []
                 : [
                   {
                     id: 'state',
-                    value: item,
-                  },
+                    value: item
+                  }
                 ])
             }}
-            textTransform="capitalize"
+            textTransform='capitalize'
           >
             {item}
           </Button>
         ))}
       </HStack>
-
-      <Divider opacity="0.2" />
-
+      <Divider opacity='0.2' />
       <PositionsTable columnFilters={columnFilters} positions={positions} />
     </Box>
   )
