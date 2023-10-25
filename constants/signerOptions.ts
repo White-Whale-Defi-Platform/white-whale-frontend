35a5bbed63398aa4f15bed12830fd0eb28efb363
nick134 <nick134-bit@proton.me>
2023-10-25 14:41:48 +0200

    chore: hardcode new gasprice
    until packages are updated

--- constants/signerOptions.ts
+++ constants/signerOptions.ts
@@ -5,11 +5,17 @@ import { ACTIVE_NETWORKS_WALLET_NAMES } from './networks'
 
 const getGasPrices = (chainName:string, chain:Chain) => {
   const activeChains = [...ACTIVE_NETWORKS_WALLET_NAMES.mainnet, ...ACTIVE_NETWORKS_WALLET_NAMES.testnet]
+
   if (activeChains.includes(chainName)) {
     const [feeTokens] = chain.fees.fee_tokens;
-    const price = feeTokens
+    let price = feeTokens
       ? feeTokens.average_gas_price || feeTokens.low_gas_price || feeTokens.fixed_min_gas_price || 0
       : 0;
+    if (chainName === 'migaloo') {
+      // Manually set to average gas price until chain registry package is updated
+      price = 2;
+    }
+
     return {
       gasPrice: GasPrice.fromString(String(price) + chain.fees.fee_tokens[0].denom),
     }
