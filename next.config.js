af492a13b98a435787c18110d65245437baa62c6
nick134 <nick134-bit@proton.me>
2023-11-06 19:21:16 +0100

    chore: update config

--- next.config.js
+++ next.config.js
@@ -93,6 +93,9 @@ const config = {
     ignoreBuildErrors: true,
   },
   output: "standalone",
+  experimental: {
+    webpackBuildWorker: true
+},
   eslint: {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
