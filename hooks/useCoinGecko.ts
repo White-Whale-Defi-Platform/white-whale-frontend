7d7133483237afe8e54d8f652820b64e4a9472b8
worrex <68468180+worrex@users.noreply.github.com>
2023-11-23 01:39:41 +0700

    fix(coingecko-geo-blocking): checking if ids are all strings

--- hooks/useCoinGecko.ts
+++ hooks/useCoinGecko.ts
@@ -9,12 +9,13 @@ const getCoinGecko = async (ids = []) => {
 }
 
 const useCoinGecko = (ids: string[]) => {
+  const areAllStrings = ids.every((element) => typeof element === 'string');
   const { data } = useQuery({
     queryKey: ['coinGecko', ids],
     queryFn: () => getCoinGecko(ids),
     refetchInterval: 60000,
     refetchIntervalInBackground: true,
-    enabled: ids?.length > 0,
+    enabled: areAllStrings && ids?.length > 0,
   })
 
   return data
