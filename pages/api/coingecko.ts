f05ba344ce5fc2d71624711e52ef4b5ec662f142
worrex <68468180+worrex@users.noreply.github.com>
2023-11-29 23:36:20 +0700

    fix(coingecko-api): updating constantly and checking if cache contains id

--- pages/api/coingecko.ts
+++ pages/api/coingecko.ts
@@ -1,6 +1,8 @@
 let cache = {}
+let initialized = false
 const fetchInterval = 60 * 1000 // 60 seconds
 const coinGeckoIds = ['white-whale', 'chihuahua-token', 'lion-dao', 'terra-luna', 'comdex', 'cosmos', 'injective-protocol', 'juno-network', 'sei-network', 'terra-luna-2']
+
 const fetchDataFromCoinGecko = async () => {
   const url = `https://api.coingecko.com/api/v3/simple/price?ids=${coinGeckoIds.join(',')}&vs_currencies=usd`;
 
@@ -19,25 +21,50 @@ const fetchDataFromCoinGecko = async () => {
     console.error('Fetch to CoinGecko API failed:', error);
     // Don't update the cache if there's an error
   }
+  return null
 };
 
 // Perform the initial fetch immediately on server start
 fetchDataFromCoinGecko().then((data) => {
-  cache = data
+  if (data) {
+    cache = data
+    initialized = true
+  }
 })
 
 setInterval(async () => {
-  await fetchDataFromCoinGecko()
+  const newData = await fetchDataFromCoinGecko();
+  if (newData) {
+    Object.keys(newData).forEach((id) => {
+      if (coinGeckoIds.includes(id)) {
+        cache[id] = newData[id];
+      }
+    })
+  }
 }, fetchInterval)
 
 export default async function handler(req, res) {
-  const { ids } = req.query
-  const idsArray = ids.split(',')
-  const filteredData = idsArray.reduce((result, id) => {
-    if (cache[id]) {
-      result[id] = cache[id];
+  if (initialized) {
+    if (Object.keys(cache).length === 0) {
+      res.status(503).json({ error: 'Service Unavailable: Cache is empty.' });
+      return
     }
-    return result;
-  }, {})
-  res.status(200).json(filteredData);
+
+    const { ids } = req.query
+    const idsArray = ids.split(',')
+    const filteredData = idsArray.reduce((result, id) => {
+      if (cache && Object.prototype.hasOwnProperty.call(cache, id)) {
+        result[id] = cache[id]
+      }
+      return result
+    }, {})
+
+    if (Object.keys(filteredData).length === 0) {
+      res.status(404).json({ error: 'No data found for the provided IDs.' })
+      return
+    }
+    res.status(200).json(filteredData)
+  } else {
+    res.status(503).json({ error: 'Cache is not initialized yet.' })
+  }
 }
