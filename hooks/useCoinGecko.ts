6c3a9c8c62c733056237576e2b9ce7c9e5c7acbe
worrex <68468180+worrex@users.noreply.github.com>
2023-12-12 14:40:22 +0700

    feat(ratio-tvl-calculated): if apis provide no data, use calculated ratio and tvl

--- hooks/useCoinGecko.ts
+++ hooks/useCoinGecko.ts
@@ -1,8 +1,11 @@
 import { useQuery } from 'react-query'
 
+import { isDevMode } from 'util/isDevMode'
+
 const getCoinGecko = async (ids = []) => {
   const newApiIds = [...new Set(ids)].filter((elem) => typeof elem === 'string').join()
-  const response = await fetch(`/api/coingecko?ids=${newApiIds}`);
+  console.log("devMode", isDevMode())
+  const response = isDevMode() ? await fetch(`https://api.coingecko.com/api/v3/simple/price?ids=${newApiIds.split(',')}&vs_currencies=usd`) : await fetch(`/api/coingecko?ids=${newApiIds}`);
   return await response.json()
 }
 
