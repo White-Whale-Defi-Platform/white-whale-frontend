745c36274b191664b358b808c0416ff4a4881100
nick134 <nick134-bit@proton.me>
2023-12-22 14:11:25 +0100

    chore: mobile wallet connect button

--- components/Wallet/Modal/WalletModal.tsx
+++ components/Wallet/Modal/WalletModal.tsx
@@ -41,11 +41,14 @@ export const WalletModal = ({ isOpen, setOpen, walletRepo }) => {
   const shouldRenderButton = (wallet: { walletName: string, isModeExtension: boolean }) => {
     const { walletName } = wallet
     const inApp = isMobile && window.leap && window.leap.mode === 'mobile-web'
-    const inj = (chainId.includes('injective') && (walletName === 'keplr-extension' || walletName === 'leap-extension')) || (chainId.includes('injective') && inApp)
+    const inj = chainId.includes('injective') && (walletName === WalletType.keplrExtension || walletName === WalletType.leapExtension)
     if (inj) {
       return true
     } else if (chainId.includes('injective') && !inApp) {
       return false
+    }
+    if (inj && inApp && walletName === WalletType.leapExtension) {
+      return true;
     }
     if (inApp && walletName === WalletType.leapExtension) {
       return true;
