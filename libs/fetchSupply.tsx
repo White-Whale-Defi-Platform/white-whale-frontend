f667e3bcad1cbe8aab862589bfebc2765a80cf67
nick134 <76399455+nick134-bit@users.noreply.github.com>
2024-02-02 00:07:02 +0100

    chore: use faster api

--- libs/fetchSupply.tsx
+++ libs/fetchSupply.tsx
@@ -1,8 +1,15 @@
+import { API_URL } from '../constants'
+
 export type FetchSupplyResponse = {
   circulating: number
   total: number
 }
 
 const url = '/api/cors?url=https://apex.migaloo.zone/api/supply'
-export const fetchSupply = async (): Promise<FetchSupplyResponse> => await (await fetch(url)).json()
+export const fetchSupply = async (): Promise<FetchSupplyResponse> => {
+  const response = await fetch(`${API_URL}/apex/supply`) || await fetch(url)
+  let data = await response.json()
+  data = data?.data ? data.data : data
+  return data
+}
 
