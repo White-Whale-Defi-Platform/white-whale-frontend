7be4d5359b9190018e93b22caefa20482a08c961
nick134 <76399455+nick134-bit@users.noreply.github.com>
2024-02-02 13:19:58 +0100

    chore: use faster api dashboard (#514)

--- util/getDashboardData.ts
+++ util/getDashboardData.ts
@@ -1,4 +1,4 @@
-import { ENIGMA_URL } from 'constants/index'
+import { API_URL, ENIGMA_URL } from 'constants/index'
 import fetch from 'isomorphic-unfetch'
 
 interface ChainInfoResponse {
@@ -8,9 +8,10 @@ interface ChainInfoResponse {
 }
 
 export const getDashboardData = async (): Promise<ChainInfoResponse[]> => {
-  const response = await fetch(`${ENIGMA_URL}/chains/dashboard`)
+  const response = await fetch(`${API_URL}/api/dashboard`) || await fetch(`${ENIGMA_URL}/chains/dashboard`)
 
-  const data : ChainInfoResponse[] = await response.json()
+  let data : ChainInfoResponse[] | any = await response.json()
+  data = data?.data ? data.data : data
   return data.map((res) => {
     if (res.chainName === 'terra-classic') {
       return {
