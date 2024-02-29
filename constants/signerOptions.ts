ddb7519b69a8d9874096dca4ed08fc80742b3e1a
w0rrex <68468180+worrex@users.noreply.github.com>
2024-02-29 12:24:21 +0100

    feat(switch-to-coinhall): coinhall default for all pools (#532)

--- constants/signerOptions.ts
+++ constants/signerOptions.ts
@@ -5,10 +5,10 @@ import { CHAIN_NAMES } from './networks'
 
 export const getGasPrices = async (chainName:string, chain:Chain) => {
   if (CHAIN_NAMES.includes(chainName)) {
-    const [feeTokens] = chain.fees.fee_tokens;
+    const [feeTokens] = chain.fees.fee_tokens
     const price = feeTokens
       ? feeTokens?.low_gas_price || feeTokens?.fixed_min_gas_price || feeTokens?.average_gas_price || 0
-      : 0;
+      : 0
     return {
       gasPrice: GasPrice.fromString(String(price) + chain.fees.fee_tokens[0]?.denom),
     }
