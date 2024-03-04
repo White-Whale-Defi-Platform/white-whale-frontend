d5a735ab0498acd469e1fc268f5c02fcf87e4d8a
nick134 <76399455+nick134-bit@users.noreply.github.com>
2024-03-04 12:07:28 +0100

    chore: increase timeouts

--- util/getDashboardData.ts
+++ util/getDashboardData.ts
@@ -8,7 +8,7 @@ interface ChainInfoResponse {
 }
 
 export const getDashboardData = async (): Promise<ChainInfoResponse[]> => {
-  const response = await fetchWithTimeout(`${await getFastestAPI()}/api/dashboard`, 5000) || await fetch(`${ENIGMA_URL}/chains/dashboard`)
+  const response = await fetchWithTimeout(`${await getFastestAPI()}/api/dashboard`, 10000) || await fetch(`${ENIGMA_URL}/chains/dashboard`)
 
   let data: ChainInfoResponse[] | any = await response.json()
   data = data?.data ? data.data : data
