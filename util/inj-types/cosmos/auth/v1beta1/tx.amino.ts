4caf54fab994e5da359e17d93dab2302cbc2e623
nick134 <76399455+nick134-bit@users.noreply.github.com>
2024-03-29 16:38:41 +0100

    feat: nicks injective package :+1: (#550)

--- util/inj-types/cosmos/auth/v1beta1/tx.amino.ts
+++
@@ -1,8 +0,0 @@
-import { MsgUpdateParams } from "./tx";
-export const AminoConverter = {
-  "/cosmos.auth.v1beta1.MsgUpdateParams": {
-    aminoType: "cosmos-sdk/x/auth/MsgUpdateParams",
-    toAmino: MsgUpdateParams.toAmino,
-    fromAmino: MsgUpdateParams.fromAmino
-  }
-};
