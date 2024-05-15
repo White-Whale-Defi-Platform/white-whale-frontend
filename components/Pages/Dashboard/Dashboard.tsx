fd3c297693c3117c5b07d86c4d915b4c9533cbaf
nick134 <76399455+nick134-bit@users.noreply.github.com>
2024-05-15 18:57:01 +0200

    chore: useWallet on dashboard for header

--- components/Pages/Dashboard/Dashboard.tsx
+++ components/Pages/Dashboard/Dashboard.tsx
@@ -12,7 +12,7 @@ import { getChainLogoUrlByName } from 'util/getChainLogoUrlByName'
 import { useGetDashboardDataAPI } from './hooks/getDashboardDataAPI'
 import { useChainInfos } from 'hooks/useChainInfo'
 import { WALLET_CHAIN_NAMES_BY_CHAIN_ID } from 'constants/networks'
-import { useChain } from '@cosmos-kit/react-lite'
+import { useWallet } from '@cosmos-kit/react-lite'
 
 export type DashboardData = {
   logoUrl: string
@@ -25,8 +25,7 @@ export type DashboardData = {
 export const Dashboard: FC = () => {
   const [dashboardState, setDashboardDataState] = useRecoilState(dashboardDataState)
   const { data: dashboardData, isLoading } = useGetDashboardDataAPI()
-  console.log(dashboardData, dashboardData?.dashboardData?.[0].chainName)
-  const tmp = useChain(dashboardData?.dashboardData?.[0].chainName || 'migaloo')
+  useWallet()
   const chains = useChainInfos()
   const prices = usePrices()
   useEffect(() => {
